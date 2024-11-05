9e281d0a400cdbb8c02ca600c6ec13b9a86fe772
nadznaf <nadziraazhanifarahiya@mail.ugm.ac.id>
2024-11-06 00:55:41 +0700

    tambah opsi dropdown item

--- resources/views/auth/layouts.blade.php
+++ resources/views/auth/layouts.blade.php
@@ -30,6 +30,9 @@
                         </a>
                         <ul class="dropdown-menu">
                             <li>
+                                <a class="dropdown-item" href="{{ route('users') }}">Users</a>
+                            </li>
+                            <li>
                                 <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
                                 document.getElementById('logout-form').submit();">Logout</a>
                                 <form id="logout-form" action="{{ route('logout') }}" method="POST">
